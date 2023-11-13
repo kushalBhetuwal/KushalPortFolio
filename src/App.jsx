882fd7f379cfc80b30a2263a74bd817d16acce39
kushal <kushalbhetuwal@gmail.com>
2023-11-13 17:15:55 +1100

    added the island and the sky

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import {Home,About,Projects,Contact} from './pages'
+const App = () => {
+  return (
+   <main className="bg-slate-300/200">
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element ={<About/>}/>
+        <Route path="/projects" element ={<Projects/>}/>
+        <Route path="/contact" element ={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
+   </main>
+  )
+}
+
+export default App
